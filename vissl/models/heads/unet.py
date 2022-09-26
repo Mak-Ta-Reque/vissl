d0973d538d20d9a012fcdccdcf2859353adab8be
Md Abdul Kadir <maktareq@gmail.com>
2022-09-26 10:43:14 +0200

    unet head is do nothing pass the input to output. Had to do it just for adpting the system

--- vissl/models/heads/unet.py
+++ vissl/models/heads/unet.py
@@ -13,21 +13,12 @@ from vissl.models.heads import register_model_head
 from vissl.config import AttrDict
 from typing import List
 
-class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-
-    def forward(self, x):
-        return self.conv(x)
 
 @register_model_head("unet_head")
 class UNet(nn.Module):
     def __init__(self, model_config: AttrDict, n_classes:int ):
         super().__init__()
-        self.outc = OutConv(64, n_classes)
 
     def forward(self, batch: torch.Tensor or List[torch.Tensor]):
-        logits = self.outc(batch)
-        return logits
+        return batch
 
